8a55e325bc42980c7ba044756ebcada8c287cfa3
hoai-duc <chauhoaiduc96@gmail.com>
2018-08-12 23:44:55 +0700

    commit all

---
+++ Panopas/Scripts/sidebar.js
@@ -0,0 +1,19 @@
+﻿/* Set the width of the side navigation to 250px */
+function openNavLeft() {
+    var width = window.innerWidth;
+    if (width >= 768 && width <= 991)
+    {
+        $(".sidebar-contain").css("width", "35%");
+    }
+    else {
+        $(".sidebar-contain").css("width", "70%");
+    }
+    
+    $(".overlay").css("width", "100%");
+    $(".overlay").css("opacity", "0.8");
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    $(".sidebar-contain").css("width", "0px");
+    $(".overlay").css("width", "0%");
+}
